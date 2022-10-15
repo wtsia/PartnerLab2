6e3168779e25c2fd22eff327a7dcfd220881554c
Winston <winstontsia@gmail.com>
2022-10-15 04:53:38 -0700

    created functional input and response

--- App.java
+++ App.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.HashMap;
 // import javax.swing.event.SwingPropertyChangeSupport;
 // import java.util.Dictionary;
 // import java.util.Hashtable;
-import java.util.HashMap;
 
 /**
  * A program that mimics ELIZA, a language processing program which returns
@@ -17,13 +17,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scnr = new Scanner(System.in);
         welcomePage();
-        // local variables
+        // variable to store phases of program execution
         int programPhase = 0;
+
         // take user input as string
         String userInput = scnr.nextLine().toString().toLowerCase();
 
-        //create dictionary for different feeling types that are associated with numbers 1-10
-        HashMap<Integer, String> userFeeling = new HashMap<>();
+        //create Hashmap for different feeling types that are associated with numbers 1-10
+        HashMap<Integer, String> userFeeling = new HashMap<Integer, String>();
         userFeeling.put(1,"depressed");
         userFeeling.put(2,"horrible");
         userFeeling.put(3,"bad");
@@ -35,6 +36,25 @@ public class App {
         userFeeling.put(9,"fantastic");
         userFeeling.put(10,"amazing");
 
+        // Hashmap that contains responses for specific keywords
+        HashMap<String, String> responseList = new HashMap<String, String>();
+        // responseList.put("","");        
+        responseList.put("know","What do you know or not know?");
+        responseList.put("","");
+        responseList.put("","");
+        responseList.put("","");
+        responseList.put("","");
+        responseList.put("","");
+        responseList.put("",""); 
+        // System.out.println("why do you feel " + userFeeling.get());
+        // System.out.println("Why do you think that is?");
+        // System.out.println("I dont quite understand, can you explain further?");
+        // System.out.println("Who made you come to me for help?");
+        // System.out.println("Is there a specific example?");
+        // System.out.println("What can you do differently?");
+        // System.out.println("So you are feeling "+ userFeeling + ", is that correct?" );
+        // System.out.println("So your "+  person + " made you come here?");
+
         /**
         * while loop takes in filtered userInput as a lowercase string
         * if userRating is an integer and meets && conditions, prints "Why do you feel ___?" and moves on
@@ -42,13 +62,8 @@ public class App {
         * @param userInput, programPhase
         */
         while(programPhase == 0) {
-            
-            while(userInput == "goodbye") {
-                System.out.println("Thank you for consulting DR.Tropic Thunder!");
-                System.exit(0);    
-            }
-
             try {
+                exitProgram(userInput);
                 int userRating = Integer.parseInt(userInput);
                 if (userRating == (int)userRating && userRating > 0 && userRating <= 10) {
                     System.out.println("Why do you feel " + userFeeling.get(userRating) + "?");
@@ -62,32 +77,32 @@ public class App {
                 userInput = scnr.nextLine().toString().toLowerCase();
                 continue;
             }
-
         }
 
         while(programPhase == 1) {
-            System.out.println(userInput + "is user input!");
-            // System.out.println("why do you feel " + userFeeling.get());
-            // System.out.println("Why do you think that is?");
-            // System.out.println("I dont quite understand, can you explain further?");
-            // System.out.println("Who made you come to me for help?");
-            // System.out.println("Is there a specific example?");
-            // System.out.println("What can you do differently?");
-            // System.out.println("So you are feeling "+ userFeeling + ", is that correct?" );
-            // System.out.println("So your "+  person + " made you come here?");
+            boolean status = true;
+            while(status == true) {
+                try {
+                    userInput = scnr.nextLine().toLowerCase();
+                    exitProgram(userInput);
+                    String stringInputArr[] = userInput.split(" ");
+                    // query hashmap response value using string keywords
+                    for (int i=0; i<stringInputArr.length; i++) {
+                        if (responseList.containsKey(stringInputArr[i]) == true) {
+                            System.out.println(responseList.get(stringInputArr[i]));
+                        } else {
+                            continue;
+                        }
+                    }
+                } catch(Exception e) {
+                    status = false;
+                    break;
+                }
+            }
         }
-        
-        // template statements for responses to feelings/user inputs
-
-		
-
-
-                
-                
-
 
     // Exit word is "Goodbye" we can change this as well
-    String exitWord = "Goodbye";
+    // String exitWord = "Goodbye";
     // if user input == "exitWord" end program and run ending print  statements
     //have different responses for different feelings, we can start with "why are you feeling ___?"
     // then we can delve into more specific questions, " >what happened that made you fell sad?"
@@ -111,6 +126,9 @@ public class App {
 
     }
 
+    /**
+     * method welcomePage() presents a menu of strings
+     */
     public static void welcomePage() {
         System.out.println("Hello my name is Dr. Tropic Thunder, and I will be your therapist today.");
         System.out.println("Today we will be talking about how you feel today.");
@@ -119,4 +137,14 @@ public class App {
         System.out.println("Let us start. How are you feeling today on a scale of 1-10?:   ");
     }
 
+    /**
+     * Takes input Element and if the string equals "goodbye", exits the program
+     * @param inputElement
+     */
+    public static void exitProgram(String inputElement) {
+        if (inputElement.equals("goodbye")) {
+            System.out.println("Thank you for consulting DR. Tropic Thunder!");
+            System.exit(0);    
+        }
+    }
 }
